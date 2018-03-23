794aefce3502f1d2b316639ad32c691b692d13aa
penry <penrychou@163.com>
2018-03-23 12:56:44 +0800

    init commit

---
+++ src/main/java/com/familly/trainingwebsite/TrainingwebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.familly.trainingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingwebsiteApplication.class, args);
+	}
+}
